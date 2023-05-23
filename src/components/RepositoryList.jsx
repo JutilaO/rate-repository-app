6d08d24f445515524937db5faf1669693d6c9e9e
JutilaO <46874717+JutilaO@users.noreply.github.com>
2023-05-23 13:29:09 +0300

    search bar

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,10 +3,11 @@ import RepositoryItem from './RepositoryItem'
 import useRepositories from '../hooks/useRepositories'
 import { Picker } from '@react-native-picker/picker'
 import { useState } from 'react'
+import TextInput from './TextInput'
+import { useDebounce } from 'use-debounce';
 
-export const RepositoryListContainer = ({ repositories, sortType, setSortType }) => {
+export const RepositoryListContainer = ({ repositories, sortType, setSortType, searchWord, setSearchWord }) => {
   const ItemSeparator = () => <View style={{ height: 8 }} />
-
 
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
@@ -17,31 +18,39 @@ export const RepositoryListContainer = ({ repositories, sortType, setSortType })
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) => <RepositoryItem data={item}/>}
-      ListHeaderComponent={<PickerComponent sortType={sortType} setSortType={setSortType}/>}
+      ListHeaderComponent={<PickerComponent sortType={sortType} setSortType={setSortType} searchWord={searchWord} setSearchWord={setSearchWord}/>}
     />
   )
 }
 
-const PickerComponent = ({ sortType, setSortType }) => {
-
+const Searchbar = ({searchWord, setSearchWord}) => {
   return (
-    <Picker
-      selectedValue={sortType}
-      onValueChange={(itemValue) => setSortType(itemValue)}>
-      <Picker.Item label="Latest repositories" value="latest" />
-      <Picker.Item label="Highest rated repositories" value="highest" />
-      <Picker.Item label="Lowest rated repositories" value="lowest" />
-    </Picker>
+    <TextInput onChangeText={(text) => setSearchWord(text)} value={searchWord} style={{backgroundColor: 'white'}} placeholder="Search" autofocus/>
   )
 }
 
-
+const PickerComponent = ({ sortType, setSortType, searchWord, setSearchWord }) => {
+  return (
+    <View>
+      <Searchbar  searchWord={searchWord} setSearchWord={setSearchWord}/>
+      <Picker
+        selectedValue={sortType}
+        onValueChange={(itemValue) => setSortType(itemValue)}>
+        <Picker.Item label="Latest repositories" value="latest" />
+        <Picker.Item label="Highest rated repositories" value="highest" />
+        <Picker.Item label="Lowest rated repositories" value="lowest" />
+      </Picker>
+    </View>
+  )
+}
 
 const RepositoryList = () => {
   const [sortType, setSortType] = useState()
-  const repositories = useRepositories(sortType)
+  const [searchWord, setSearchWord] = useState('')
+  const [searchKeyword] = useDebounce(searchWord, 1500)
+  const repositories = useRepositories(sortType, searchKeyword)
 
-  return <RepositoryListContainer repositories={repositories} sortType={sortType} setSortType={setSortType}/>
+  return <RepositoryListContainer repositories={repositories} sortType={sortType} setSortType={setSortType} searchWord={searchWord} setSearchWord={setSearchWord}/>
 }
 
 export default RepositoryList
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,13 +1,13 @@
 import { useQuery } from '@apollo/client'
 import { GET_REPOSITORIES } from '../graphql/queries'
 
-const useRepositories = (sort) => {
+const useRepositories = (sort, searchKeyword) => {
 
   let sortType = sort === 'latest' ? 'CREATED_AT' : 'RATING_AVERAGE'
   let sortDirection = sort === 'lowest' ? 'ASC' : 'DESC'
 
-  const repositories = useQuery(GET_REPOSITORIES, { variables: { orderDirection: sortDirection, orderBy: sortType }, fetchPolicy: 'cache-and-network' })
-  if(!repositories.loading){
+  const repositories = useQuery(GET_REPOSITORIES, { variables: { orderDirection: sortDirection, orderBy: sortType, searchKeyword }, fetchPolicy: 'cache-and-network' })
+  if(repositories && !repositories.loading && repositories.data){
     return repositories.data.repositories
   }
 }
